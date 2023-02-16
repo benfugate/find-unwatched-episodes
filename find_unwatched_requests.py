17b7bb296babb5e254ce9b4ce85318a42ff14dba
benfugate <benjamin_fugate13@yahoo.com>
2023-02-16 16:41:49 -0500

    Organize code into a class

--- find_unwatched_requests.py
+++ find_unwatched_requests.py
@@ -7,116 +7,138 @@ import argparse
 from tqdm import tqdm
 from datetime import datetime, timedelta
 
-with open("config.json") as f:
-    defaults = json.load(f)
-parser = argparse.ArgumentParser()
-parser.add_argument("--overseerr-url", default=defaults["overseerr_url"], help="overseerr host url")
-parser.add_argument("--overseerr-token", default=defaults["overseerr_token"], help="overseerr api token")
-parser.add_argument("--tautulli-url", default=defaults["tautulli_url"], help="tautulli host url")
-parser.add_argument("--tautulli-token", default=defaults["tautulli_token"], help="tautulli api token")
-parser.add_argument("--radarr-token", default=defaults["radarr_token"], help="radarr api token")
-parser.add_argument("--sonarr-token", default=defaults["sonarr_token"], help="sonarr api token")
-args = parser.parse_args()
-args.tautulli_url = args.tautulli_url.rstrip("/")
+class FindUnwatchedRequests:
+    def __init__(self):
+        with open("config.json") as f:
+            defaults = json.load(f)
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--overseerr-url", default=defaults["overseerr_url"], help="overseerr host url")
+        parser.add_argument("--overseerr-token", default=defaults["overseerr_token"], help="overseerr api token")
+        parser.add_argument("--tautulli-url", default=defaults["tautulli_url"], help="tautulli host url")
+        parser.add_argument("--tautulli-token", default=defaults["tautulli_token"], help="tautulli api token")
+        self.args = parser.parse_args()
+        self.args.tautulli_url = self.args.tautulli_url.rstrip("/")
+        self.radarr_token = None
+        self.sonarr_token = None
 
-def delete_content(delete_series=False, delete_movies=False):
-    series = None
-    movies = None
-    sonarr_host = None
-    radarr_host = None
-    for result in results:
-        if delete_series and "series" in result[3]:
-            sonarr_host = re.search(r"http.*?://[^/]*", result[3]).group(0)
-            series_request = f"{sonarr_host}/api/v3/series/?apikey={args.sonarr_token}"
-            series = json.loads(requests.get(series_request).text)
-        elif delete_movies and "movie" in result[3]:
-            radarr_host = re.search(r"http.*?://[^/]*", result[3]).group(0)
-            movies_request = f"{radarr_host}/api/v3/movie/?apikey={args.radarr_token}"
-            movies = json.loads(requests.get(movies_request).text)
+        self.unwatched_requests = []
+        self.sonarr_host = None
+        self.radarr_host = None
+        self.all_content = None
+        self.delete = []
 
-    if delete_series and not sonarr_host or not series:
-        delete_series = False
-    if delete_movies and not radarr_host or not movies:
-        delete_movies = False
+    def _overseerr_request(self, endpoint):
+        headers = {
+            "Content-type": "application/json",
+            "x-api-key": self.args.overseerr_token
+        }
+        return json.loads(requests.get(f"{endpoint}", headers=headers).text)
 
-    for result in results:
-        id = None
-        endpoint = result[3].split("/")[-1]
-        if "/series/" in result[3]:
-            if not delete_series:
-                continue
-            for show in series:
-                if show["titleSlug"] == endpoint:
-                    id = show["id"]
-            if id:
-                delete_url = f"{sonarr_host}/api/v3/series/{id}?apikey={args.sonarr_token}&deleteFiles=true"
+
+    def _find_management_hosts(self):
+        def add_scheme(host):
+            if host["useSsl"]:
+                host["scheme"] = "https://"
             else:
-                print(f"Failed to delete {result[0]}")
-                continue
-        elif "/movie/" in results[3]:
-            if not delete_movies:
-                continue
-            endpoint = result[3].split("/")[-1]
+                host["scheme"] = "http://"
+            return host
+        sonarr_host = add_scheme(self._overseerr_request(f"{self.args.overseerr_url}/api/v1/settings/sonarr")[0])
+        radarr_host = add_scheme(self._overseerr_request(f"{self.args.overseerr_url}/api/v1/settings/radarr")[0])
+        self.sonarr_host = f"{sonarr_host['scheme']}{sonarr_host['hostname']}:{sonarr_host['port']}"
+        self.radarr_host = f"{radarr_host['scheme']}{radarr_host['hostname']}:{radarr_host['port']}"
+        self.sonarr_token = sonarr_host["apiKey"]
+        self.radarr_token = radarr_host["apiKey"]
+
+    def _grab_content_library(self):
+        self._find_management_hosts()
+        all_series, all_movies = {}, {}
+        if "series" in self.delete:
+            series_request = f"{self.sonarr_host}/api/v3/series/?apikey={self.sonarr_token}"
+            all_series = json.loads(requests.get(series_request).text)[0]
+        if "movies" in self.delete:
+            movies_request = f"{self.radarr_host}/api/v3/movie/?apikey={self.radarr_token}"
+            all_movies = json.loads(requests.get(movies_request).text)[0]
+        self.all_content = {**all_series, **all_movies}
+
+    def _get_host_info(self, url):
+        content_type = re.search(r"(movies|series)", url).group(1)
+        if content_type == "movies":
+            return {"host": self.radarr_host, "token": self.radarr_token}
+        elif content_type == "series":
+            return {"host": self.sonarr_host, "token": self.sonarr_token, "content_type": content_type}
+
+    def delete_content(self):
+        self._grab_content_library()
+        for request in self.unwatched_requests:
             id = None
-            for movie in movies:
-                if movie["titleSlug"] == endpoint:
-                    id = movie["id"]
-            if id:
-                delete_url = f"{radarr_host}/api/v3/movie/{id}?apikey={args.radarr_token}&deleteFiles=true"
+            endpoint = request[3].split("/")[-1]
+            if bool([content for content in self.delete if(content in request[3])]):
+                host = self._get_host_info(request[3])
+                for content in self.all_content:
+                    if content["titleSlug"] == endpoint:
+                        id = content["id"]
+                if id:
+                    delete_url = f"{host['host']}/api/v3/series/{host['content_type']}?apikey={host['token']}&deleteFiles=true"
+                else:
+                    print(f"Failed to delete {request[0]}")
+                    continue
             else:
-                print(f"Failed to delete {result[0]}")
                 continue
 
-        response = requests.delete(delete_url)
-        if response.status_code == 200:
-            print(f"Deleted {result[0]}")
-        else:
-            print(f"Failed to delete {result[0]}")
+            response = requests.delete(delete_url)
+            if response.status_code == 200:
+                print(f"Deleted {request[0]}")
+            else:
+                print(f"Failed to delete {request[0]}")
 
-overseerr_request = f"{args.overseerr_url}/api/v1/request?take=500&filter=available"
-headers = {
-    "Content-type": "application/json",
-    "x-api-key": args.overseerr_token
-}
 
-response = requests.get(f"{overseerr_request}", headers=headers)
-plex_requests = json.loads(response.text)
-results = []
-for plex_request in tqdm(plex_requests["results"]):
-    mediaAddedAt = plex_request["media"]["mediaAddedAt"]
-    requestedBy = plex_request["requestedBy"]["plexUsername"]
-    oneMonthAgo = (datetime.utcnow() - timedelta(days=30))
-    if mediaAddedAt:
-        mediaAddedAt = datetime.strptime(mediaAddedAt, "%Y-%m-%dT%H:%M:%S.%f%z").replace(tzinfo=None)
+    def display_unwatched_requests(self):
+        if not self.unwatched_requests:
+            print("No unwatched content found :)")
+            exit()
 
-    if not mediaAddedAt or mediaAddedAt > oneMonthAgo:
-        continue
+        print("| {:60} | {:20} | {:30} | {:100} |".format(*["Title", "Requested By", "Date Available", "Management URL"]))
+        print("-"*223)
+        for request in self.unwatched_requests:
+            print("| {:60} | {:20} | {:30} | {:100} |".format(*request))
 
-    tautulli_request = f"{args.tautulli_url}/api/v2?apikey={args.tautulli_token}&cmd=get_item_watch_time_stats&rating_key={plex_request['media']['ratingKey']}"
-    response = requests.get(tautulli_request)
-    watch_data = json.loads(response.text)["response"]["data"]
-    watch_number = [item["total_plays"] for item in watch_data]
-    if not any(watch_number):
-        response = requests.get(f"{args.tautulli_url}/api/v2?apikey={args.tautulli_token}&cmd=get_metadata&rating_key={plex_request['media']['ratingKey']}")
-        try:
-            title = json.loads(response.text)["response"]["data"]["title"]
-        except Exception as e:
-            continue  # This show was probably recently deleted, found on overseerr but not on tautulli
-        results.append([title, requestedBy, str(mediaAddedAt), plex_request["media"]["serviceUrl"]])
 
-results = sorted(results, key=lambda l: l[2])  # Sort by oldest request to most recent
-if not results:
-    print("No unwatched content found :)")
-    exit()
+    def find_unwatched_requests(self):
+        request_url = f"{self.args.overseerr_url}/api/v1/request?take=500&filter=available"
+        plex_requests = self._overseerr_request(request_url)
+        results = []
+        for plex_request in tqdm(plex_requests["results"]):
+            media_added_at = plex_request["media"]["mediaAddedAt"]
+            media_requested_by = plex_request["requestedBy"]["plexUsername"]
+            one_month_ago = (datetime.utcnow() - timedelta(days=30))
+            if media_added_at:
+                media_added_at = datetime.strptime(media_added_at, "%Y-%m-%dT%H:%M:%S.%f%z").replace(tzinfo=None)
 
-print("| {:60} | {:20} | {:30} | {:100} |".format(*["Title", "Requested By", "Date Available", "Management URL"]))
-print("-"*223)
-for result in results:
-    print("| {:60} | {:20} | {:30} | {:100} |".format(*result))
+            if not media_added_at or media_added_at > one_month_ago:
+                continue
 
-delete_movies, delete_series = False, False
-if args.radarr_token and input(f"Delete all unwatched movies? Y/N: ").lower() == "y":
-    delete_series = True
-if args.sonarr_token and input(f"Delete all unwatched shows? Y/N: ").lower() == "y":
-    delete_movies = True
-delete_content(delete_series, delete_movies)
+            tautulli_request = f"{self.args.tautulli_url}/api/v2?apikey={self.args.tautulli_token}&cmd=get_item_watch_time_stats&rating_key={plex_request['media']['ratingKey']}"
+            response = requests.get(tautulli_request)
+            watch_data = json.loads(response.text)["response"]["data"]
+            watch_number = [item["total_plays"] for item in watch_data]
+            if not any(watch_number):
+                response = requests.get(f"{self.args.tautulli_url}/api/v2?apikey={self.args.tautulli_token}&cmd=get_metadata&rating_key={plex_request['media']['ratingKey']}")
+                try:
+                    title = json.loads(response.text)["response"]["data"]["title"]
+                except Exception as e:
+                    continue  # This show was probably recently deleted, found on overseerr but not on Tautulli
+                results.append([title, media_requested_by, str(media_added_at), plex_request["media"]["serviceUrl"]])
+
+        self.unwatched_requests = sorted(results, key=lambda l: l[2])  # Sort by oldest request to most recent
+
+if __name__ == '__main__':
+    find_unwatched = FindUnwatchedRequests()
+    find_unwatched.find_unwatched_requests()
+    find_unwatched.display_unwatched_requests()
+
+    if find_unwatched.args.radarr_token and input(f"Delete all unwatched movies? Y/N: ").lower() == "y":
+        find_unwatched.delete.append("movies")
+    if find_unwatched.args.sonarr_token and input(f"Delete all unwatched shows? Y/N: ").lower() == "y":
+        find_unwatched.delete.append("series")
+    if find_unwatched.delete:
+        find_unwatched.delete_content()
